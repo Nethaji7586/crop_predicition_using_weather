442527d228a286b678347041b2c3e58a14eb39f4
Nethaji <nethajisiva1503@gmail.com>
2024-11-02 14:17:27 +0530

    flast1

--- crop_recommendation/settings.py
+++ crop_recommendation/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gmdy^mhufqi77hf91*g@78^)s-+=eyq*24b0)gt29-*b-_1546'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
