f0da0d38c7381d021d3bebaad98f7910390f60b5
Nethaji <nethajisiva1503@gmail.com>
2024-11-02 14:02:48 +0530

    final1

--- crop_recommendation/settings.py
+++ crop_recommendation/settings.py
@@ -110,10 +110,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "crops/static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
