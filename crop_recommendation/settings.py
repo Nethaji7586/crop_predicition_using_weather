b02b28f856963a5a3ec8dae5d2aec6de3b870144
Nethaji <nethajisiva1503@gmail.com>
2024-11-02 14:12:34 +0530

    finall1

--- crop_recommendation/settings.py
+++ crop_recommendation/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gmdy^mhufqi77hf91*g@78^)s-+=eyq*24b0)gt29-*b-_1546'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
